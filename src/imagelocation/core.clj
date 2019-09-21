6684116889af1610df534ae8a25db029edc786fb
Brad Lucas <brad@beaconhill.com>
2019-09-21 09:36:51 -0400

    command line processing

--- src/imagelocation/core.clj
+++ src/imagelocation/core.clj
@@ -9,25 +9,38 @@
 
 
 (def cli-options
-  [["-f" "--filename FILENAME" "Process FILENAME for location information"]
+  [["-w" "--web" "Start web app"]
    ["-h" "--help" "Show help usage"]])
 
 (defn usage [summary]
-  (->> ["Image Location"
-        ""
+  (->> ["usage: imagelocation.jar [options] [filename[s]]"
+        "options:"
         summary
-        ""]
+        ""
+        "Examples:"
+        "java -jar imagelocation.jar -w           ;; start web app"
+        "java -jar imagelocation.jar photo.jpg    ;; process single file"
+        "java -jar imagelocation.jar *.jpg        ;; process all jpg files"
+        ""
+        "Output from command line processing:"
+        "filename,lat,lng,link"
+        ""
+        ]
        (str/join \newline)))
 
 (defn process-file [filename]
-  (pp/pprint (format "Processing %s" filename))
-  (pp/pprint (image/process filename)))
+  (let [r (image/process filename)]
+    (format "%s,%s,%s,%s" filename (:lat r) (:lng r) (:link r))))
 
+(defn process-files [filenames]
+  (let [lst (map #(process-file %) filenames)]
+    (doseq [l lst]
+      (println l))))
 
 (defn -main [& args]
-  ;; (config/load-config)
   (let [{:keys [options arguments errors summary]} (parse-opts args cli-options)]
-    (cond
-      (:filename options) (process-file (:filename options))
-      (:help options) (println (usage summary))
-      :else (jetty/run-jetty handler/app {:port 4002}))))
+    (if (> (count arguments) 0)
+      (process-files arguments)
+      (cond
+        (:help options) (println (usage summary))
+        :else (jetty/run-jetty handler/app {:port 4002})))))
